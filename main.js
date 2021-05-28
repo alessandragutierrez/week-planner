f7074555a20b11248d969b5011cbdbda29add31e
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-28 16:59:40 -0700

    day views in progress.

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var $modal = document.querySelector('.modal');
 var $entryForm = document.querySelector('.entry-form');
 var $tableTime = document.querySelector('.time');
 var $tableDesc = document.querySelector('.desc');
+var $dayOfWeek = document.querySelector('.day-title');
+var $dayButton = document.querySelectorAll('.day-button');
 var i;
 
 $entryButton.addEventListener('click', openModal);
@@ -21,7 +23,7 @@ $modal.addEventListener('click', closeModal);
 function submitEntry(event) {
   event.preventDefault();
   saveEntry(event);
-  checkEmpty();
+  addNew();
 }
 
 function saveEntry(event) {
@@ -89,19 +91,34 @@ function addNew() {
   $tableDesc.lastElementChild.appendChild(newEntry);
 }
 
-function checkEmpty() {
-  var $td = document.querySelectorAll('td');
-  for (i = 0; i < $td.length; i++) {
-    if ($td[i].innerText !== '') {
-      addNew();
-    } else {
-      console.log('hi');
-    }
-  }
-}
+// function checkEmpty(newEntry) {
+//   var $td = document.querySelectorAll('td');
+//   for (i = 0; i < $td.length; i++) {
+//     if ($td[i].innerText !== undefined) {
+//       addNew();
+//       break;
+//     } else {
+//       $td[i].textContent = newEntry.desc;
+//     }
+//   }
+// }
 
 // check if all the <td>s are empty
 // query select all the table names
 // loop through the node list and check if innertext of the <td>s are empty,
 // if not empty, run the functions from earlier. If empty, update the test value
 // of the <td>s instead of creating new ones =-)
+
+document.addEventListener('click', changeDay);
+function changeDay(event) {
+  if (event.target.matches('.day-button') !== true) {
+    return;
+  }
+  var day = event.target.getAttribute('data-day');
+  var dayCap;
+  function capitalizeFirstLetter() {
+    dayCap = day.charAt(0).toUpperCase() + day.slice(1).toLowerCase();
+  }
+  capitalizeFirstLetter();
+  $dayOfWeek.textContent = ('Schedule events for ' + dayCap);
+}
