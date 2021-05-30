c31dd7543e100ebbed8f0c75ee00d76003b005ae
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-29 20:15:41 -0700

    Open the modal containing the entry form when the user clicks the 'add entry' button. #1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 /* global data */
+
+var $addButton = document.querySelector('.add-button');
+var $modal = document.querySelector('.modal');
+
+$addButton.addEventListener('click', openModal);
+
+function openModal(event) {
+  $modal.classList.remove('hidden');
+}
