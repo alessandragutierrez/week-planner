0f01750fde3c130d23c4e24c2ae52ecd527ced18
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-29 20:19:08 -0700

    Close the modal when the user clicks 'submit' on the entry form. #1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@
 
 var $addButton = document.querySelector('.add-button');
 var $modal = document.querySelector('.modal');
+var $entryForm = $modal.firstElementChild;
 
 $addButton.addEventListener('click', openModal);
-
 function openModal(event) {
   $modal.classList.remove('hidden');
+}
+
+$entryForm.addEventListener('submit', submitEntry);
+function submitEntry(event) {
+  closeModal();
+}
+
+function closeModal() {
+  $modal.classList.add('hidden');
 }
