69a807f1b8bc5defc1c7c7a3ea9e155a95e27dc9
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-29 21:19:27 -0700

    When the submit button is clicked, save entry data to loca storage. #1

--- js/main.js
+++ js/main.js
@@ -2,18 +2,44 @@
 
 var $addButton = document.querySelector('.add-button');
 var $modal = document.querySelector('.modal');
-var $entryForm = $modal.firstElementChild;
+var $entryForm = $modal.querySelector('.entry-form');
 
 $addButton.addEventListener('click', openModal);
+$entryForm.addEventListener('submit', submitEntry);
+
 function openModal(event) {
+  if (event.target.matches('.add-button') !== true) {
+    return;
+  }
   $modal.classList.remove('hidden');
 }
 
-$entryForm.addEventListener('submit', submitEntry);
 function submitEntry(event) {
+  event.preventDefault();
   closeModal();
+  saveEntry();
+  data.nextEntryId = data.nextEntryId + 1;
 }
 
 function closeModal() {
   $modal.classList.add('hidden');
+}
+
+function saveEntry() {
+  var newEntry = {
+    day: $entryForm.elements.day.value,
+    hour: $entryForm.elements.hour.value,
+    AMPM: $entryForm.elements.AMPM.value,
+    desc: $entryForm.elements.desc.value,
+    entryId: data.nextEntryId
+  };
+  findDay(newEntry);
+}
+
+function findDay(newEntry) {
+  for (var key in data.entries) {
+    if (newEntry.day === key) {
+      data.entries[key].push(newEntry);
+    }
+  }
 }
