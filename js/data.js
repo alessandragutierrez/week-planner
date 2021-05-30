193f17fe5901e93c9a58c53a13708d8700b3f27d
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-29 20:28:05 -0700

    Create a data model which will store calendar data in local storage. #1

--- js/data.js
+++ js/data.js
@@ -1,5 +1,26 @@
 /* exported data */
 
 var data = {
-
+  entries: {
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: []
+  },
+  nextEntryId: 1,
+  edit: null,
+  currentDay: null
 };
+
+var previousDataJSON = localStorage.getItem('calendar-data');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('calendar-data', dataJSON);
+});
