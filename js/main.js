2e252b3143d0881495b26d76e9868957cd5e0133
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-29 23:41:20 -0700

    Create a table for each daily schedule. Create a DOM tree for each entry, and append it to the table. #2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var $addButton = document.querySelector('.add-button');
 var $modal = document.querySelector('.modal');
 var $entryForm = $modal.firstElementChild;
+var $tbody = document.querySelector('tbody');
 
 $addButton.addEventListener('click', openModal);
 $modal.addEventListener('click', cancelOpenModal);
@@ -44,4 +45,34 @@ function findDay(newEntry) {
       data.entries[key].push(newEntry);
     }
   }
+}
+
+//
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  addEntriesToPage();
+});
+
+function renderEntry(newEntry) {
+  var $tr = document.createElement('tr');
+  var $tdTime = document.createElement('td');
+  var $tdDesc = document.createElement('td');
+
+  $tdTime.textContent = newEntry.hour + ' ' + newEntry.AMPM;
+  $tdDesc.textContent = newEntry.desc;
+
+  $tr.appendChild($tdTime);
+  $tr.appendChild($tdDesc);
+
+  return $tr;
+}
+
+function addEntriesToPage() {
+  for (var key in data.entries) {
+    for (var i = 0; i < data.entries[key].length; i++) {
+      var entryValues = renderEntry(data.entries[key][i]);
+      $tbody.appendChild(entryValues);
+    }
+  }
+
 }
