1924a4f5605843be68f892de9ed1fc174ac37fed
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-30 21:29:26 -0700

    When a new entry is made, the event will automatically appear on the bottom of the schedule without having to refresh the page. #2

--- js/main.js
+++ js/main.js
@@ -5,82 +5,15 @@ var $modal = document.querySelector('.modal');
 var $entryForm = $modal.firstElementChild;
 var $dayTitle = document.querySelector('.day-title');
 var $tables = document.querySelectorAll('table');
+var newEntry;
 var entriesOrganized;
 var i;
 
+window.addEventListener('DOMContentLoaded', addEntriesToPage);
 document.addEventListener('click', changeDay);
 $addButton.addEventListener('click', openModal);
 $modal.addEventListener('click', cancelOpenModal);
 $entryForm.addEventListener('submit', submitEntry);
-
-function changeDay(event) {
-  if (event.target.matches('.day-button') !== true) {
-    return;
-  }
-  changeDayTitle(event);
-  changeTable(event);
-}
-
-function changeDayTitle(event) {
-  var day = event.target.getAttribute('data-day');
-  data.view = day;
-  var dayCap = day.charAt(0).toUpperCase() + day.slice(1);
-  $dayTitle.textContent = 'Scheduled events for ' + dayCap;
-}
-
-function changeTable(event) {
-  var day = event.target.getAttribute('data-day');
-  for (i = 0; i < $tables.length; i++) {
-    if ($tables[i].getAttribute('data-day') !== day) {
-      $tables[i].classList.add('hidden');
-    } else {
-      $tables[i].classList.remove('hidden');
-    }
-  }
-}
-
-function openModal(event) {
-  $modal.classList.remove('hidden');
-}
-
-function cancelOpenModal(event) {
-  if (event.target.matches('.modal') === true) {
-    $modal.classList.add('hidden');
-  }
-}
-
-function submitEntry(event) {
-  event.preventDefault();
-  saveEntry();
-  closeModal();
-  data.nextEntryId = data.nextEntryId + 1;
-}
-function saveEntry() {
-  var newEntry = {
-    day: $entryForm.elements.day.value,
-    hour: $entryForm.elements.hour.value,
-    AMPM: $entryForm.elements.AMPM.value,
-    desc: $entryForm.elements.desc.value,
-    entryId: data.nextEntryId
-  };
-  findDay(newEntry);
-}
-function findDay(newEntry) {
-  for (var key in data.entries) {
-    if (newEntry.day === key) {
-      data.entries[key].push(newEntry);
-    }
-  }
-}
-function closeModal() {
-  $modal.classList.add('hidden');
-}
-
-//
-
-window.addEventListener('DOMContentLoaded', function (event) {
-  addEntriesToPage();
-});
 
 function renderEntry(newEntry) {
   var $tr = document.createElement('tr');
@@ -163,4 +96,82 @@ function moveTwelvesToStart(arrayX) {
     }
   }
   return arrayX;
+}
+
+function changeDay(event) {
+  if (event.target.matches('.day-button') !== true) {
+    return;
+  }
+  changeDayTitle(event);
+  changeTable(event);
+}
+
+function changeDayTitle(event) {
+  var day = event.target.getAttribute('data-day');
+  data.view = day;
+  var dayCap = day.charAt(0).toUpperCase() + day.slice(1);
+  $dayTitle.textContent = 'Scheduled events for ' + dayCap;
+}
+
+function changeTable(event) {
+  var day = event.target.getAttribute('data-day');
+  for (i = 0; i < $tables.length; i++) {
+    if ($tables[i].getAttribute('data-day') !== day) {
+      $tables[i].classList.add('hidden');
+    } else {
+      $tables[i].classList.remove('hidden');
+    }
+  }
+}
+
+function openModal(event) {
+  $modal.classList.remove('hidden');
+}
+
+function cancelOpenModal(event) {
+  if (event.target.matches('.modal') === true) {
+    $modal.classList.add('hidden');
+  }
+}
+
+function submitEntry(event) {
+  event.preventDefault();
+  saveEntry();
+  addEntryToPage();
+  closeModal();
+  data.nextEntryId = data.nextEntryId + 1;
+}
+
+function saveEntry() {
+  newEntry = {
+    day: $entryForm.elements.day.value,
+    hour: $entryForm.elements.hour.value,
+    AMPM: $entryForm.elements.AMPM.value,
+    desc: $entryForm.elements.desc.value,
+    entryId: data.nextEntryId
+  };
+  findDay(newEntry);
+}
+
+function findDay(newEntry) {
+  for (var key in data.entries) {
+    if (newEntry.day === key) {
+      data.entries[key].push(newEntry);
+    }
+  }
+}
+
+function addEntryToPage() {
+  var entryValues = renderEntry(newEntry);
+  var entryDay = newEntry.day;
+  for (i = 0; i < $tables.length; i++) {
+    var tableDay = $tables[i].getAttribute('data-day');
+    if (entryDay === tableDay) {
+      $tables[i].lastElementChild.appendChild(entryValues);
+    }
+  }
+}
+
+function closeModal() {
+  $modal.classList.add('hidden');
 }
